6b6c01013a055476272a1c2f2a32a26705637a93
liu3zhenlab <liu3zhen@ksu.edu>
2025-04-19 12:11:41 -0500

    update

--- installation/install.sh
+++ installation/install.sh
@@ -4,6 +4,6 @@ cd ~/venvs/
 virtualenv minicLSTM
 source ~/venvs/minicLSTM/bin/activate
 # requirement
-requirements=/bulk/liu3zhen/research/projects/mini_prediction/main_minicLSTM/installation/requirements.txt
+requirements=requirements.txt
 pip install -r $requirements 
 
